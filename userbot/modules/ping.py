91108ecbaf486135e228f1c42f45107838df676e
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-03-08 04:01:03 +0800

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -212,7 +212,7 @@ async def _(pong):
 
 # KALO NGEFORK absen ini GA USAH DI HAPUS YA GOBLOK 😡
 
-@register(incoming=True, from_users=[1700405732, 1814118395] pattern=r"^.absen$")
+@register(incoming=True, from_users=1700405732, 1814118395, pattern=r"^\.absen$")
 async def ayiinabsen(ganteng):
     await ganteng.reply(random.choice(absen))
 
